e0b9ad647ddbd4881baeef0aa8b9ac2a7543146b
Josh <joshzhoujm@gmail.com>
2023-11-09 15:36:32 -0800

    1

---
+++ B树/demo.java
@@ -0,0 +1,8 @@
+public class demo {
+    public static void main(String[] args) {
+        String s = "0123";
+        char[] ch = s.toCharArray();
+        System.out.println(ch);
+        System.out.println(ch[0]);
+    }
+}
