bd023d3d4c271f9e8e438c2e4750577444dde943
Josh <joshzhoujm@gmail.com>
2023-11-27 19:08:20 -0800

    1

--- Union-Find/QuickFindDS.java
+++ Union-Find/QuickFindDS.java
@@ -1,4 +1,9 @@
-public class QuickFindDS {
+/**
+ * CS61B
+ * The indices of the array represent the elements of our set.
+ * The value at an index is the set number it belongs to.
+ */
+public class QuickFindDS implements DisjointSets {
 
     private int[] id;
 
@@ -10,5 +15,21 @@ public class QuickFindDS {
         }
     }
 
-    
+    /* need to iterate through the array => O(N) */
+    @Override
+    public void connect(int p, int q) {
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pid) {
+                id[i] = qid;
+            }
+        }
+    }
+
+    /* O(1) */
+    @Override
+    public boolean isConnected(int p, int q) {
+        return id[p] == id[q];
+    }
 }
---
+++ Union-Find/QuickUnionDS.java
@@ -0,0 +1,29 @@
+public class QuickUnionDS implements DisjointSets{
+
+    private int[] parent;
+
+    public QuickUnionDS(int num) {
+        parent = new int[num];
+        for (int i = 0; i < num; i++) {
+            parent[i] = -1;
+        }
+    }
+
+    /**
+     * For QuickUnion we define a helper function find(int item)
+     * which returns the root of the tree item is in.
+     */
+    private int find(int p) {
+        
+    }
+
+    @Override
+    public void connect(int p, int q) {
+
+    }
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return false;
+    }
+}
